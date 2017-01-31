d65c728d74f664aa471f70b3cb4d6121cc5eabeb
bill hoang <billhoang79@gmail.com>
2017-01-31 13:13:50 -0800

    updating code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-angular.module('app', []);
+var app = angular.module('app', ['controllers']);
+//Wrong way to dec
+// var MainController = function($scope) {
+//	$scope.val = "re23"	
+//	}
+
+// app.controller('MainController', function($scope) {
+//   $scope.val = "test456"
+//})
+
+
+//Production way to declare modules, creates dependency, be broken into own files
+angular.module('controllers', []).controller('MainController', function($scope) {
+	$scope.val = "test789"
+})
+
